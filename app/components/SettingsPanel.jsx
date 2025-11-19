3cea143d4ee5b30cd977b32de0d00103be1bf1e9
DerailleurAgile <chris.chapman@derailleurconsulting.com>
2025-11-19 13:55:11 -0500

    UPDATE: Change width and background color for Bible version hint.

--- app/components/SettingsPanel.jsx
+++ app/components/SettingsPanel.jsx
@@ -116,10 +116,11 @@ export default function SettingsPanel({
 
                 {/* Tooltip */}
                 {showTooltip && (
-                  <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-80 bg-gray-800 text-gray-100 text-xs rounded-md p-2 shadow-lg z-50 opacity-0 animate-fadeIn">
-                    Make sure to download the selected translation in your Bible.com app!
-                  </div>
+                    <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-96 bg-gray-700 border border-gray-600 text-gray-100 text-xs rounded-md p-3 shadow-xl z-50 opacity-0 animate-fadeIn">
+                        Make sure to download the selected translation in your Bible.com app!
+                    </div>
                 )}
+
               </div>
             </label>
 
