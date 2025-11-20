cc2b879d8f9f1e4e277b116643702bb3be0577e3
DerailleurAgile <chris.chapman@derailleurconsulting.com>
2025-11-20 13:38:04 -0500

    CHORE: bump version to 0.3.4 Alpha in package.json

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,9 @@ import { version } from './package.json';
 const nextConfig: NextConfig = {
   compress: true, // Enable gzip compression
 
+  // Expose the app version to the client-side code
   env: {
-    NEXT_PUBLIC_APP_VERSION: version, // expose version to client
+    NEXT_PUBLIC_APP_VERSION: version.trim(),
   },
 };
 
