3d4f9709a627f782d0f3162e8064498f972b9d0c
DerailleurAgile <chris.chapman@derailleurconsulting.com>
2025-11-24 13:23:51 -0500

    FIX: Set lectionary json max-age to 0 to allow changes to propagate.

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=86400, must-revalidate',
+            value: 'public, max-age=0, must-revalidate',
           },
         ],
       },
