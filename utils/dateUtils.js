bd157bfc40f9a10add16d39316c2587144782b20
DerailleurAgile <chris.chapman@derailleurconsulting.com>
2026-06-18 11:38:40 -0400

    REFACTOR: removed commented and unused code

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -1,13 +1,3 @@
-// Format dateKey (MM-DD) to display format
-// export function formatDateKey(dateKey) {
-//   const [month, day] = dateKey.split('-');
-//   const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
-//                       'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-//   const monthName = monthNames[parseInt(month, 10) - 1];
-//   const year = new Date().getFullYear();
-//   return `${monthName} ${parseInt(day, 10)}, ${year}`;
-// }
-
 // If dateKey is "12-25", this should format it with the VIEWING year, not current year
 export function formatDateKey(dateKey, year = new Date().getFullYear()) {
   const [month, day] = dateKey.split('-').map(Number);
